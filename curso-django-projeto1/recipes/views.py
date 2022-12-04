e276d070f91225a66f49b6c000c82069191afa6d
Kahilo Pedro <pedrojogad1o@gmail.com>
2022-12-04 16:38:04 +0100

    t

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -6,3 +6,6 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name':'Kahilo pedro Massango - Inserido usando context '
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
